0b11eb2a05bb3ffc231f3f81b4c3d78ffd3a86c3
Melluo <hubgnmelluo01@gmail.com>
2026-08-16 19:32:24 -0300

    Add: Adicionando exercício 8

--- Vs - Lógica, Algoritmos e Programação/DESAFIO PRÁTICO 1/A -8.py
+++ Vs - Lógica, Algoritmos e Programação/DESAFIO PRÁTICO 1/A -8.py
@@ -3,3 +3,15 @@
 • R$ 12,00 por dia, se o número de diárias for igual a 7;
 • R$ 16,50 por diária, se o número de diárias for menor que 7.
 Você deve pedir a informação de quantos dias o hóspede ficou hospedado. Construa um código que mostre o nome do hóspede e o total da conta a pagar.'''
+nome_hospede = input("DIGITE O NOME DO HÓSPEDE: ").capitalize()
+qtd_diaria = int(input("DIGITE A QUANTIDADE DE DIAS HOSPEDADO: ")) # 290 + dias 
+
+if qtd_diaria > 7:
+    valor_cobrado = 290 + (6.50 * qtd_diaria)
+    print(f"O hóspede {nome_hospede} terá que pagar R$ {valor_cobrado:.2f}.")
+elif qtd_diaria == 7:
+    valor_cobrado = 290 + (12 * qtd_diaria)
+    print(f"O hóspede {nome_hospede} terá que pagar R$ {valor_cobrado:.2f}.")
+else:
+    valor_cobrado = 290 + (16.50 * qtd_diaria)
+    print(f"O hóspede {nome_hospede} terá que pagar R$ {valor_cobrado:.2f}.")
