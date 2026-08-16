645d696265cc4b3665014c7fc93036819edd7e1e
Melluo <hubgnmelluo01@gmail.com>
2026-08-16 19:51:35 -0300

    Add: Adicionando exercício 5

--- Vs - Lógica, Algoritmos e Programação/DESAFIO PRÁTICO 1/A -5.py
+++ Vs - Lógica, Algoritmos e Programação/DESAFIO PRÁTICO 1/A -5.py
@@ -2,3 +2,16 @@
 • Δ > 0: Duas raízes reais distintas.
 • Δ = 0: Uma raiz real (ou duas raízes reais iguais).
 • Δ < 0: Nenhuma raiz real (duas raízes complexas).'''
+print("DETERMINANDO A QUANTIDADE DE RAIZES DE UM DELTA")
+coeficiente_a = float(input("DIGITE O COEFICIENTE A: "))
+coeficiente_b = float(input("DIGITE O COEFICIENTE B: "))
+coeficiente_c = float(input("DIGITE O COEFICIENTE C: "))
+
+delta = (coeficiente_b ** 2) - (4 * coeficiente_a * coeficiente_c)
+
+if delta > 0:
+    print(f"O delta tem um valor maior que 0 ({delta}) e possui duas raízes reais distintas!")
+elif delta == 0:
+    print(f"O delta tem um valor igual a 0 ({delta}) e possui uma raiz real!")
+else:
+    print(f"O delta tem um valor menor que 0 ({delta}) e não possui nenhuma raiz real!")
